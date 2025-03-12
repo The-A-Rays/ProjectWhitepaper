43a0e29fa9bdc5fb3a57edf3d4e5cfa2fbb24472
rak <koenigryan4@gmail.com>
2025-03-12 15:17:05 +0000

    Simpler setup
    Setup springboot and maven to be simpler from the get go, hopefully allowing for a more compact project

---
+++ aicomics/src/main/java/com/project/aicomics/AicomicsApplication.java
@@ -0,0 +1,13 @@
+package com.project.aicomics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicomicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicomicsApplication.class, args);
+	}
+
+}
---
+++ aicomics/src/test/java/com/project/aicomics/AicomicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.aicomics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicomicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
