38a9e31abcefb3a80f4027cb32d29b35d291f0e6
rak <koenigryan4@gmail.com>
2025-03-14 11:38:37 +0000

    Created ConfigurationFIle class
    Is currently missing testing, but the base class is down and is a very simple singleton object, so I am expecting very few issues

--- aicomics/src/main/java/com/project/aicomics/ConfigurationFile.java
+++ aicomics/src/main/java/com/project/aicomics/ConfigurationFile.java
@@ -1,33 +1,72 @@
 package com.project.aicomics;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 
+/**
+ * Singleton class used to access the configuration file data. 
+ * It's a singleton as we should only ever access this once per running attempt
+ */
 public class ConfigurationFile {
-    
-    
-    public static String getAPIKey() {
-        try {
-            File apikey = new File("src\\main\\resources\\apikey.txt");
-            String key;
-            try (Scanner read = new Scanner(apikey)) {
-                for(int i = 0; i < 6; i++) {read.nextLine();}
-                key = read.nextLine();
-            }
-            return key.substring(8, key.length()-1);
 
-        } catch(FileNotFoundException e) {
-            System.out.println("File not found, please check config file");
+    private static ConfigurationFile INSTANCE = null;
+    String completionsURL;
+    String embedURL;
+    String modelURL;
+    String org;
+    String apiKey;
+    String model;
+
+    private ConfigurationFile() {
+        File apiconfig = new File("src\\main\\resources\\apikey.txt");
+        try (Scanner read = new Scanner(apiconfig)) {
+            completionsURL = read.nextLine().substring(16);
+            embedURL = read.nextLine().substring(15);
+            modelURL = read.nextLine().substring(11);
+            org = read.nextLine().substring(8);
+            apiKey = read.nextLine().substring(8);
+            model = read.nextLine().substring(6);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found exception: Please check API config file");
         }
-        return "";
+
+        // Very simple checks to ensure the URLs are placed in the correct position
+        if (completionsURL.charAt(0) != 'h') System.out.println("completionsURL not read correctly. Check API config file");
+        if (embedURL.charAt(0) != 'h') System.out.println("embed URL not read correctly. Check API config file");
+        if (modelURL.charAt(0) != 'h') System.out.println("model URL not read correctly. Check API config file");
+    }
+    
+    /**
+     * @return Created or returns the single ConfigurationFile instance
+     */
+    public static synchronized ConfigurationFile getInstance() {
+        if (INSTANCE == null) {INSTANCE = new ConfigurationFile();}
+        return INSTANCE;
     }
 
-    public static String getCompletionsURL() {
-
+    public String getCompURL() {
+        return completionsURL;
     }
 
-    public static String getModel() {
+    public String getEmbedURL() {
+        return embedURL;
+    }
 
+    public String getModelURL() {
+        return modelURL;
+    }
+
+    public String getOrg() {
+        return org;
+    }
+
+    public String getAPIKey() {
+        return apiKey;
+    }
+
+    public String getModel() {
+        return model;
     }
 }
