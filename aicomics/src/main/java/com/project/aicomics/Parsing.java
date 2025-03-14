582ea4aa0e115dc583dbe787e24a7dfeba94cec3
EmaFE <ema-elena.florea@ucdconnect.ie>
2025-03-14 13:58:20 +0000

    parsing numbered list
    used regex to achieve this

---
+++ aicomics/src/main/java/com/project/aicomics/Parsing.java
@@ -0,0 +1,32 @@
+package com.project.aicomics;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parsing {
+  
+    public ArrayList<String> parseNumberedList(String numberedList){
+        ArrayList<String> parsedList = new ArrayList<>();             
+                    
+        Pattern p = Pattern.compile("(\\d{1,2}\\.\\s)([\\w\\W]+?)(?=\\n\\d{1,2}\\.\\s|$)");
+        Matcher m = p.matcher(numberedList);
+
+        while (m.find()){
+            parsedList.add(m.group(2));
+        }
+       for (int i = 0; i < parsedList.size(); i++){
+            parsedList.add(parsedList.get(i));
+            System.out.println(parsedList.get(i));
+       }
+       return parsedList;
+      }
+
+
+    //TODO  
+    public String JSONtoString(String JSONResponse){
+
+      return "";
+    }
+    }
+
