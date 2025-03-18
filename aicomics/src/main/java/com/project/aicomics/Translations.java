ce9f034868cc010d31c6684ca6930713152eec99
EmaFE <ema-elena.florea@ucdconnect.ie>
2025-03-18 20:33:25 +0000

    Translations class

--- aicomics/src/main/java/com/project/aicomics/Translations.java
+++ aicomics/src/main/java/com/project/aicomics/Translations.java
@@ -1,30 +1,60 @@
 package com.project.aicomics;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 import com.project.aicomics.service.OpenAIService;
 
 public class Translations {
 
-    private enum languages {Spanish, English};
+    private enum languages {spanish, english};
     private OpenAIService ai = new OpenAIService();
-    private FileOutputStream vignettes;
+    private String translatedFile;
 
-    public Translations() {
-        try {vignettes = new FileOutputStream("English.tsv");
-        } catch (FileNotFoundException e) {}
+    public Translations(languages originalLanguage, languages targetLanguage) {
+        this.translatedFile = originalLanguage.toString().toLowerCase() + "-" + targetLanguage.toString().toLowerCase() + ".tsv";
     }
 
     //TODO 
     /**
      * Gets the translation of a given vignette
      * Check language pair if it has the translation already
-     * If pair not found prompt to get it, then write to file
-     */
-    public getTranslation() {
+     * If pair not found, prompt to get it, then write to file
+          * @throws IOException 
+          */
+         public String getTranslation(Vignette input, String targetLanguage) throws IOException {
+        String textToBeTranslated = input.getCombinedText() + input.getLeftText();
+        String translatedString = ai.TranslateText(textToBeTranslated);
 
+        File file = new File(translatedFile);
+
+        //add string to newly created file
+        if (file.createNewFile()){
+            FileWriter wr = new FileWriter(file);
+            wr.write(textToBeTranslated);
+            wr.write(System.lineSeparator());
+            wr.write(translatedString);
+            wr.write(System.lineSeparator());
+        } else { //if file already exists, check weather it already has the translation
+            boolean exists = false;
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                if (sc.nextLine().equals(translatedString)){
+                    exists = true;
+                    break;
+                }
+            } //if not, add it
+            if (!exists){
+                //appends
+                FileWriter wr = new FileWriter(file, true);
+                wr.write(textToBeTranslated);
+                wr.write(System.lineSeparator());
+                wr.write(translatedString);
+                wr.write(System.lineSeparator());
+            }
+        }
+        return null;
     }
-
-
 }
