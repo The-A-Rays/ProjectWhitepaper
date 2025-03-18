f395e91b378bfb3d30f1ff717f01dbe26cc6b0a6
ConorK97 <83973567+ConorK97@users.noreply.github.com>
2025-03-18 14:21:39 +0000

    added Vignette.java

---
+++ aicomics/src/main/java/com/project/aicomics/Vignette.java
@@ -0,0 +1,26 @@
+package com.project.aicomics;
+
+public class Vignette {
+
+    private String leftPose, combinedText, leftText, rightPose, background;
+
+    public String getBackground() {
+        return this.background;
+    }
+    public String getCombinedText() {
+        return combinedText;
+    }
+    public String getLeftPose() {
+        return leftPose;
+    }
+    public String getLeftText() {
+        return leftText;
+    }
+    public String getRightPose() {
+        return rightPose;
+    }
+
+    public Vignette(){
+
+    }
+}
