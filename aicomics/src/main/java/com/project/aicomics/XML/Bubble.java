b6c7e1af2d7cb8eaa3e95b29f8a89075de86c91a
ConorK97 <83973567+ConorK97@users.noreply.github.com>
2025-04-03 10:00:10 +0100

    New folder for XML logic / classes

--- aicomics/src/main/java/com/project/aicomics/XML/Bubble.java
+++ aicomics/src/main/java/com/project/aicomics/XML/Bubble.java
@@ -1,4 +1,4 @@
-package com.project.aicomics;
+package com.project.aicomics.XML;
 
 public class Bubble {
   
--- aicomics/src/main/java/com/project/aicomics/XML/Figure.java
+++ aicomics/src/main/java/com/project/aicomics/XML/Figure.java
@@ -1,4 +1,4 @@
- package com.project.aicomics;
+ package com.project.aicomics.XML;
 
 // import java.awt.Color;
 // import java.util.Arrays;
