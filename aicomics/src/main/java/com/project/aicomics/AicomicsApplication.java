fd5f4b3c80ee9da98b95a5ffc345037462cb20c2
EmaFE <ema-elena.florea@ucdconnect.ie>
2025-04-10 12:24:59 +0100

    Update AicomicsApplication.java

--- aicomics/src/main/java/com/project/aicomics/AicomicsApplication.java
+++ aicomics/src/main/java/com/project/aicomics/AicomicsApplication.java
@@ -1,15 +1,12 @@
 package com.project.aicomics;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.project.aicomics.Translations.Language.spanish;
-
-import java.util.List;
-
 import com.project.aicomics.XML.XMLFile;
-import com.project.aicomics.controller.DevController;
 import com.project.aicomics.service.OpenAIService;
 
 @RestController
@@ -18,8 +15,6 @@ public class AicomicsApplication {
 
 	public static void main(String[] args) {
 		OpenAIService ai = new OpenAIService();
-		DevController.status("Status test haha");
-		DevController.error("error working", new Exception("this is an error"));
 		SpringApplication.run(AicomicsApplication.class, args);
 
 
